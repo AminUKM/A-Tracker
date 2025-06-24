9c43f4e6d44bf2f40688040525c21cd18adb5675
Ajrul Amin <148028869+AminUKM@users.noreply.github.com>
2025-06-24 23:29:30 +0800

    Update edit_profile.php

--- template/edit_profile.php
+++ template/edit_profile.php
@@ -1,7 +1,7 @@
 <?php
 include 'db.php';
 session_start();
-if (!isset($_SESSION['matric'])) { header("Location: login.php"); exit(); }
+if (!isset($_SESSION['matric'])) { header("Location: Login.php"); exit(); }
 
 // Fetch current student profile
 $stmt = $conn->prepare("SELECT * FROM Table_Students WHERE matric_number = ?");
